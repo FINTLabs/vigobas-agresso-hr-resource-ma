6e118f5eefddb799aa2ad299c6ffbda65ffaa0a2
Bernt Johnsen <39300332+berntjoh@users.noreply.github.com>
2022-03-01 14:57:29 +0100

    Update AssemblyInfo.cs
    Added  AssemblyVersion and AssemblyFileVersion

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -12,11 +12,11 @@ using System.Runtime.CompilerServices;
 // associated with an assembly.
 //
 [assembly: AssemblyTitle("Agresso HR MA")]
-[assembly: AssemblyDescription("Management agent for å hente ut ressurser fra Agresso HR")]
+[assembly: AssemblyDescription("Management agent for Ã¥ hente ut ressurser fra Agresso HR")]
 
 [assembly: AssemblyCompany("VIGO IKS")]
 [assembly: AssemblyProduct("VigoBAS")]
-[assembly: AssemblyCopyright("Copyright © 2022 VigoIKS")]
+[assembly: AssemblyCopyright("Copyright (C) 2022 VigoIKS")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,8 +31,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyInformationalVersion("1.7.0")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
